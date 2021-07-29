730b0f7ea4233e957fcac7ba27bf32d70cb21ab2
James Whelan <35579071+azurehotdog@users.noreply.github.com>
2021-07-28 18:59:20 -0700

    Wording changes

--- app.py
+++ app.py
@@ -2,24 +2,21 @@ from flask import Flask, redirect, url_for, request
 from flask import render_template
 app = Flask(__name__)
 
-@app.route('/reg')
-def subscribe():
-    return render_template("register.html")
-
-@app.route('/')
-def comingsoon():
-    return render_template("comingsoon.html")
-
 
 @app.route('/success')
 def success():
     return render_template("thankyou.html")
 
-@app.route('/overview')
-def overview():
-    return render_template("trackoverview.html")
+@app.route('/')
+def subscribe():
+    return render_template("subscribe.html")
 
-@app.route('/privacy')
-def privacy():
-    return render_template("privacy.html")    
+@app.route('/unsubscribe')
+def unsubscribe():
+    return render_template("unsubscribe.html")
+
+@app.route('/unsubscribesuccess')
+def unsubscribesuccess():
+    return render_template("sorrytoseeyougo.html")
+
 
